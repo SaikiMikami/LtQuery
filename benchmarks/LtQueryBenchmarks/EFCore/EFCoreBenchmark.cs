acf1f769a344a60eb1f0f0c47ca267070550eb57
Saiki Mikami <maikami@likesapphire.jp>
2025-06-17 06:04:05 +0900

    #53 Upgrade SDK => .NET8.0

--- benchmarks/LtQueryBenchmarks/EFCore/EFCoreBenchmark.cs
+++ benchmarks/LtQueryBenchmarks/EFCore/EFCoreBenchmark.cs
@@ -12,22 +12,14 @@ class EFCoreBenchmark : AbstractBenchmark
 
         using (var context = new TestContext())
         {
-            _selectSingle(context);
-            _selectSimple(context).ToArray();
-            _selectIncludeChilren(context, 20).ToArray();
-            _selectComplex(context, "PLCJKJKRUK", 10, 20).ToArray();
+            SelectSingle();
+            SelectSimple();
+            SelectIncludeChilren();
+            SelectComplex();
             context.Add(new Tag("a"));
             context.SaveChanges();
         }
     }
-    static readonly Func<TestContext, Blog> _selectSingle
-        = EF.CompileQuery((TestContext context) => context.Set<Blog>().Single(_ => _.Id == 1));
-    static readonly Func<TestContext, IEnumerable<Blog>> _selectSimple
-        = EF.CompileQuery((TestContext context) => context.Set<Blog>().OrderBy(_ => _.Id).Take(20).AsNoTracking());
-    static readonly Func<TestContext, int, IEnumerable<Blog>> _selectIncludeChilren
-        = EF.CompileQuery((TestContext context, int id) => context.Set<Blog>().Include(_ => _.Posts).Where(_ => _.Id < id).AsNoTracking());
-    static readonly Func<TestContext, string, int, int, IEnumerable<Blog>> _selectComplex
-        = EF.CompileQuery((TestContext context, string userName, int skipCount, int takeCount) => context.Set<Blog>().Include(_ => _.User).Include(_ => _.Posts).ThenInclude(_ => _.User).Where(_ => _.Posts.Any(_ => _.User!.Name == userName)).OrderBy(_ => _.Id).Skip(skipCount).Take(takeCount).AsNoTracking());
 
     public void Cleanup()
     {
@@ -39,7 +31,7 @@ class EFCoreBenchmark : AbstractBenchmark
         Blog entity;
         using (var context = new TestContext())
         {
-            entity = _selectSingle(context);
+            entity = context.Set<Blog>().Single(x => x.Id == 1);
         }
 
         var accum = 0;
@@ -52,7 +44,7 @@ class EFCoreBenchmark : AbstractBenchmark
         Blog[] entities;
         using (var context = new TestContext())
         {
-            entities = _selectSimple(context).ToArray();
+            entities = context.Set<Blog>().OrderBy(_ => _.Id).Take(20).ToArray();
         }
 
         var accum = 0;
@@ -68,7 +60,7 @@ class EFCoreBenchmark : AbstractBenchmark
         Blog[] entities;
         using (var context = new TestContext())
         {
-            entities = _selectIncludeChilren(context, 20).ToArray();
+            entities = context.Set<Blog>().Include(_ => _.Posts).Where(_ => _.Id < 20).ToArray();
         }
 
         var accum = 0;
@@ -105,7 +97,7 @@ class EFCoreBenchmark : AbstractBenchmark
         Blog[] entities;
         using (var context = new TestContext())
         {
-            entities = _selectComplex(context, "PLCJKJKRUK", 10, 20).ToArray();
+            entities = context.Set<Blog>().Include(_ => _.User).Include(_ => _.Posts).ThenInclude(_ => _.User).Where(_ => _.Posts.Any(_ => _.User!.Name == "PLCJKJKRUK")).OrderBy(_ => _.Id).Skip(10).Take(20).ToArray();
         }
 
         var accum = 0;
